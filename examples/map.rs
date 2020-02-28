e85c610c6999e8a97c7ea8343211d5d3547c5755
Sven Nilsen <bvssvni@gmail.com>
2020-02-28 10:01:08 +0100

    Fixed map example

--- examples/map.rs
+++ examples/map.rs
@@ -4,11 +4,7 @@ use reachability_solver::{shapes, map};
 
 fn main() {
     let dim = [3, 3];
-    // let shape = shapes::diag_rect(dim);
-    let shape = vec![
-        [0, 4], [3, 1]
-        // [0, 4], [4, 0], [3, 1]
-    ];
+    let shape = shapes::diag_rect(dim);
     let map = map::map2(dim, &shape);
     println!("{}", map::visualize(&map));
 }
